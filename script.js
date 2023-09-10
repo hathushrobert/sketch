deaaea4d740b129a9bade751ad08567c76f9d60a
Hathush Robert <hathushrobert458@gmail.com>
2023-09-10 19:27:35 +0530

    Added hover
    Grid blackens on hover. Added size button

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let body = document.querySelector('body');
 let gridContainer = document.querySelector('.grid-container');
 
 // Create 16 x 16 grids
-let gridBox;
 
 for (let i = 0; i < 256; i++){
 
-    gridBox = document.createElement('div');
+    let gridBox = document.createElement('div');
     gridBox.className = 'grid-box';
+    gridBox.id = "square-" + i;
+    gridBox.addEventListener('mouseover', ()=>{
+        gridBox.style['background-color'] = 'black';
+
+    });
     gridContainer.appendChild(gridBox);
 }
